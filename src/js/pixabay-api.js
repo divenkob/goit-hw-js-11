75939d9176ba4272d6f03805e94312826cce1ade
divenkob <kryvenko.di@gmail.com>
2024-05-12 11:42:29 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ export function getImage(searchValue) {
     const API_KEY = '43825908-e00f2a501e8ab3b01ba78a8cd';
     const params = `?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
   
-    return get(`${BASE_URL}${params}`)
+    return fetch(`${BASE_URL}${params}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
